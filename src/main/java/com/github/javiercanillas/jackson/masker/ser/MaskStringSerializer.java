e41ffb4ebc2bf50cddf9ae7883f3ce135d4852b3
Javier Canillas <javier.canillas@recargapay.com>
2021-06-11 13:47:22 -0300

    Complete refactor for better support over collections

--- src/main/java/com/github/javiercanillas/jackson/masker/ser/MaskStringSerializer.java
+++ src/main/java/com/github/javiercanillas/jackson/masker/ser/MaskStringSerializer.java
@@ -4,85 +4,142 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
-import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.fasterxml.jackson.databind.ser.PropertyWriter;
+import com.fasterxml.jackson.databind.util.NameTransformer;
 import com.github.javiercanillas.jackson.masker.MaskUtils;
 import com.github.javiercanillas.jackson.masker.annotation.MaskString;
 import com.github.javiercanillas.jackson.masker.view.Masked;
+import lombok.Getter;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
 
-/**
- * Custom Masking Serialization for fields annotated with {@link MaskString}. It uses internally the implementation of
- * {@link StringSerializer}
- */
-public class MaskStringSerializer extends StdScalarSerializer<String> implements ContextualSerializer {
+public class MaskStringSerializer extends JsonSerializer<Object> implements ContextualSerializer {
 
-    private static final long serialVersionUID = -5753691330908523181L;
-
-    private final StringSerializer defaultSerializer;
+    @Getter
+    private final JsonSerializer<Object> nonMaskerSerializer;
+    @Getter
     private final int keepLastCharacters;
+    @Getter
     private final char maskCharacter;
 
+    /**
+     * Jackson requires me to have a non-argument constructor but this instance seems never used directly for
+     * serialization.
+     */
     public MaskStringSerializer() {
-        this(MaskString.DEFAULTS_KEEP_LAST_CHARACTERS, MaskString.DEFAULTS_MASK_CHARACTER);
+        this(null, MaskString.DEFAULTS_KEEP_LAST_CHARACTERS, MaskString.DEFAULTS_MASK_CHARACTER);
     }
 
-    public MaskStringSerializer(final int keepLastCharacters, final char maskCharacter) {
-        super(String.class, false);
-        defaultSerializer = new StringSerializer();
+    public MaskStringSerializer(final JsonSerializer<Object> nonMaskerSerializer, final int keepLastCharacters, final char maskCharacter) {
+        this.nonMaskerSerializer = nonMaskerSerializer;
         this.keepLastCharacters = keepLastCharacters;
         this.maskCharacter = maskCharacter;
     }
 
     @Override
-    public boolean isEmpty(final SerializerProvider prov, final String value) {
-        return defaultSerializer.isEmpty(prov, value);
+    public JsonSerializer<Object> unwrappingSerializer(NameTransformer unwrapper) {
+        return wrappedResultOrElseNull(() -> this.nonMaskerSerializer.unwrappingSerializer(unwrapper));
     }
 
     @Override
-    public void serialize(final String value, final JsonGenerator gen, final SerializerProvider provider) throws IOException {
-        if (Masked.isEnabled(provider)) {
-            gen.writeString(MaskUtils.mask(value, this.keepLastCharacters, this.maskCharacter));
-        } else {
-            defaultSerializer.serialize(value, gen, provider);
-        }
+    public JsonSerializer<Object> replaceDelegatee(JsonSerializer<?> delegatee) {
+        return wrappedResultOrElseNull(() -> this.nonMaskerSerializer.replaceDelegatee(delegatee));
     }
 
     @Override
-    public void serializeWithType(final String value, final JsonGenerator gen, final SerializerProvider provider,
-                                  final TypeSerializer typeSer) throws IOException {
-        if (Masked.isEnabled(provider)) {
-            gen.writeString(MaskUtils.mask(value, this.keepLastCharacters, this.maskCharacter));
-        } else {
-            defaultSerializer.serializeWithType(value, gen, provider, typeSer);
-        }
+    public JsonSerializer<Object> withFilterId(Object filterId) {
+        return wrappedResultOrElseNull(() -> this.nonMaskerSerializer.withFilterId(filterId));
     }
 
     @Override
-    public JsonNode getSchema(final SerializerProvider provider, final Type typeHint) {
-        return defaultSerializer.getSchema(provider, typeHint);
+    public void serialize(final Object value, final JsonGenerator gen, final SerializerProvider serializers) throws IOException {
+        this.nonMaskerSerializer.serialize(mask(value, serializers), gen, serializers);
     }
 
     @Override
-    public void acceptJsonFormatVisitor(final JsonFormatVisitorWrapper visitor, final JavaType typeHint)
-            throws JsonMappingException {
-        defaultSerializer.acceptJsonFormatVisitor(visitor, typeHint);
+    public void serializeWithType(Object value, JsonGenerator gen, SerializerProvider serializers,
+                                  TypeSerializer typeSer) throws IOException {
+        this.nonMaskerSerializer.serializeWithType(mask(value, serializers), gen, serializers, typeSer);
     }
 
     @Override
-    public JsonSerializer<?> createContextual(final SerializerProvider prov, final BeanProperty property) {
+    public boolean isUnwrappingSerializer() {
+        return this.nonMaskerSerializer.isUnwrappingSerializer();
+    }
+
+    @Override
+    public boolean isEmpty(SerializerProvider provider, Object value) {
+        return this.nonMaskerSerializer.isEmpty(provider, value);
+    }
+
+    @Override
+    public Class<Object> handledType() {
+        return this.nonMaskerSerializer.handledType();
+    }
+
+    @Override
+    public boolean usesObjectId() {
+        return this.nonMaskerSerializer.usesObjectId();
+    }
+
+    @Override
+    public JsonSerializer<?> getDelegatee() {
+        return wrappedResultOrElseNull(() -> this.nonMaskerSerializer.getDelegatee());
+    }
+
+    @Override
+    public Iterator<PropertyWriter> properties() {
+        return this.nonMaskerSerializer.properties();
+    }
+
+    @Override
+    public void acceptJsonFormatVisitor(JsonFormatVisitorWrapper visitor, JavaType type) throws JsonMappingException {
+        this.nonMaskerSerializer.acceptJsonFormatVisitor(visitor, type);
+    }
+
+    @Override
+    public JsonSerializer<?> createContextual(final SerializerProvider serializers, final BeanProperty property) throws JsonMappingException {
         var annotation = Optional.ofNullable(property).map(prop -> prop.getAnnotation(MaskString.class));
-        return new MaskStringSerializer(Math.max(0,
-                annotation.map(MaskString::keepLastCharacters).orElse(MaskString.DEFAULTS_KEEP_LAST_CHARACTERS)),
+        return new MaskStringSerializer(serializers.findValueSerializer(property.getType(), property),
+                Math.max(0, annotation.map(MaskString::keepLastCharacters).orElse(MaskString.DEFAULTS_KEEP_LAST_CHARACTERS)),
                 annotation.map(MaskString::maskCharacter).orElse(MaskString.DEFAULTS_MASK_CHARACTER));
+    }
+
+    private Object mask(Object value, SerializerProvider serializers) {
+        Object newValue;
+        if (Masked.isEnabled(serializers)) {
+            if (value instanceof String) {
+                newValue = MaskUtils.mask((String) value, this.keepLastCharacters, this.maskCharacter);
+            } else if (value.getClass().isArray()) {
+                newValue = MaskUtils.mask((String[]) value, this.keepLastCharacters, this.maskCharacter);
+            } else if (value instanceof List) {
+                newValue = MaskUtils.mask((List<String>) value, this.keepLastCharacters, this.maskCharacter);
+            } else if (value instanceof Set) {
+                newValue = MaskUtils.mask((Set<String>) value, this.keepLastCharacters, this.maskCharacter);
+            } else {
+                // ups!! value type is not supported :(
+                newValue = value;
+            }
+        } else {
+            newValue = value;
+        }
+        return newValue;
+    }
+
+    private JsonSerializer<Object> wrappedResultOrElseNull(final Supplier<JsonSerializer<?>> supplier) {
+        return Optional.ofNullable(supplier.get())
+                .map(ser -> (JsonSerializer<Object>) ser)
+                .map(ser -> new MaskStringSerializer(ser, this.keepLastCharacters, this.maskCharacter))
+                .orElse(null);
     }
 }
---
+++ src/test/java/com/github/javiercanillas/jackson/masker/examples/ObjectWithStringSetFieldTest.java
@@ -0,0 +1,70 @@
+package com.github.javiercanillas.jackson.masker.examples;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javiercanillas.jackson.masker.annotation.MaskString;
+import com.github.javiercanillas.jackson.masker.view.Masked;
+import lombok.Data;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Masking {@Link java.util.Set} might be tricky, because it might appear duplications over masked values.
+ * Please be aware of this.
+ */
+class ObjectWithStringSetFieldTest {
+
+    @Data
+    public static class TestObject {
+
+        private Set<String> stringValues;
+        @MaskString
+        private Set<String> sensitiveStrings;
+        @MaskString(keepLastCharacters = 6)
+        private Set<String> sensitiveStringKeepLastCharacters;
+        @MaskString(keepLastCharacters = 6, maskCharacter = '#')
+        private Set<String> sensitiveStringKeepLastCharactersWithCustomMask;
+    }
+    private static TestObject buildTestObject() {
+        return new TestObject();
+    }
+    private static TestObject buildTestObject(Set<String> stringValues) {
+        var obj = buildTestObject();
+        obj.setSensitiveStrings(stringValues);
+        obj.setSensitiveStringKeepLastCharacters(stringValues);
+        obj.setSensitiveStringKeepLastCharactersWithCustomMask(stringValues);
+        obj.setStringValues(stringValues);
+        return obj;
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(buildTestObject(Set.of("aabbccdd", "eeffgghh")),
+                        "{\"stringValues\":[\"eeffgghh\",\"aabbccdd\"],\"sensitiveStrings\":[\"********\"],\"sensitiveStringKeepLastCharacters\":[\"**ffgghh\",\"**bbccdd\"],\"sensitiveStringKeepLastCharactersWithCustomMask\":[\"##bbccdd\",\"##ffgghh\"]}",
+                        "{\"stringValues\":[\"eeffgghh\",\"aabbccdd\"],\"sensitiveStrings\":[\"eeffgghh\",\"aabbccdd\"],\"sensitiveStringKeepLastCharacters\":[\"eeffgghh\",\"aabbccdd\"],\"sensitiveStringKeepLastCharactersWithCustomMask\":[\"eeffgghh\",\"aabbccdd\"]}"),
+                Arguments.of(buildTestObject(Set.of("aabb", "ccdd")),
+                        "{\"stringValues\":[\"ccdd\",\"aabb\"],\"sensitiveStrings\":[\"****\"],\"sensitiveStringKeepLastCharacters\":[\"aabb\",\"ccdd\"],\"sensitiveStringKeepLastCharactersWithCustomMask\":[\"aabb\",\"ccdd\"]}",
+                        "{\"stringValues\":[\"ccdd\",\"aabb\"],\"sensitiveStrings\":[\"ccdd\",\"aabb\"],\"sensitiveStringKeepLastCharacters\":[\"ccdd\",\"aabb\"],\"sensitiveStringKeepLastCharactersWithCustomMask\":[\"ccdd\",\"aabb\"]}"),
+                Arguments.of(buildTestObject(),
+                        "{\"stringValues\":null,\"sensitiveStrings\":null,\"sensitiveStringKeepLastCharacters\":null,\"sensitiveStringKeepLastCharactersWithCustomMask\":null}",
+                        "{\"stringValues\":null,\"sensitiveStrings\":null,\"sensitiveStringKeepLastCharacters\":null,\"sensitiveStringKeepLastCharactersWithCustomMask\":null}")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void map(TestObject obj, String maskedStringOutput, String normalStringOutput) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        var expectedMaskObject = mapper.readValue(maskedStringOutput, TestObject.class);
+        var expectedNormalObject = mapper.readValue(normalStringOutput, TestObject.class);
+
+        Assertions.assertEquals(expectedMaskObject, mapper.readValue(mapper.writerWithView(Masked.class).writeValueAsString(obj), TestObject.class));
+        Assertions.assertEquals(expectedNormalObject, mapper.readValue(mapper.writer().writeValueAsString(obj), TestObject.class));
+    }
+}
